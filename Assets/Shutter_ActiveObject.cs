f422efeaf61e76859df147cc8c50339911f7364e
sin49 <ngo533gute@gmail.com>
2024-08-16 00:41:06 +0900

    123213
    213213

--- Assets/Shutter_ActiveObject.cs
+++ Assets/Shutter_ActiveObject.cs
@@ -7,7 +7,7 @@ public class Shutter_ActiveObject : Shutter
     [Header("활성화되기 까지의 시간 이걸로 에니메이션과 맞추샘")]
     public float initTIme = 0.15f;
     float inittimer;
-    [Header("활성화 오브젝트 위치")]
+    [Header("활성화 오브젝트 위치(활성화 오브젝트와 같은 위치여도 상관 없음)")]
     public Transform ActiveTransform;
     [Header("활성화 오브젝트")]
     public GameObject ACtiveObject;
