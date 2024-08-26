ee0dc667c271ce16a01aebe7cf8565e1d8113434
sin49 <ngo533gute@gmail.com>
2024-08-26 13:58:53 +0900

    1231
    1231231231

--- Assets/Events/Output/DescriptionEvent.cs
+++ Assets/Events/Output/DescriptionEvent.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DescriptionEvent : OutputEvent
 {
     public DeAcrtiveOnTimer Description;
     public float time;
-
+    public string message;
     public override void output()
     {
         base.output();
         Description.time = time;
+        if (Description.GetComponent<TextMeshProUGUI>())
+        {
+            Description.GetComponent<TextMeshProUGUI>().text = message;
+        }
         Description.gameObject.SetActive(true);
     }
 }
