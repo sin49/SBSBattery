3b111f724414caba9d7f2c1a014d137ca04f26bc
sunsss123 <81277071+sunsss123@users.noreply.github.com>
2024-08-27 16:39:29 +0900

    41231231
    42131124

--- Assets/Events/Output/KillAreaOutputEvent.cs
+++ Assets/Events/Output/KillAreaOutputEvent.cs
@@ -7,6 +7,19 @@ public class KillAreaOutputEvent : OutputEvent
     public Collider killCollider;
     public override void output()
     {
+        EnemyKillOnArea();
         base.output();
+    }
+
+    public void EnemyKillOnArea()
+    {
+        if (killCollider != null)
+        {
+            if (killCollider.gameObject.CompareTag("Enemy"))
+            {
+                Enemy enemy = killCollider.gameObject.GetComponent<Enemy>();
+                enemy.Dead();
+            }
+        }
     }
 }
--- Assets/Events/Output/TeleportOutputEvent.cs
+++ Assets/Events/Output/TeleportOutputEvent.cs
@@ -5,20 +5,97 @@ using UnityEngine;
 public class TeleportOutputEvent : OutputEvent
 {
     public Transform teleportPoint;
+    public Transform point;
+    public List<Transform> pointGroup = new List<Transform>();
+
+    [Header("pointGroup")]
+    public bool previousPointNum;
+    public bool nextPointNum;
+    [Header("이전/다음 텔포 트랜스폼")]
+    public bool previousTelpoNum;
+    public bool nextTelpoNum;
+
+    int pointNum, telpoNum;
+
     public override void output()
     {
-        throw new System.NotImplementedException();
+        base.output();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        ChangePoint();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        if (!Application.isPlaying)
+            ChangePoint();
+    }
+    public void PlayerTeleport()
+    {
+        if (teleportPoint != null)
+        {
+            if (PlayerHandler.instance != null && PlayerHandler.instance.CurrentPlayer != null)
+            {
+                PlayerHandler.instance.CurrentPlayer.transform.position = teleportPoint.position;
+            }
+        }
+    }
+
+    public void ChangePoint()
+    {
+        if (previousPointNum)
+        {
+            previousPointNum = false;
+            pointNum--;
+            InitPoint();
+        }
+
+        if (nextPointNum)
+        {
+            nextPointNum = false;
+            pointNum++;
+            InitPoint();
+        }
+
+        if (previousTelpoNum)
+        {
+            previousTelpoNum = false;
+            telpoNum--;
+            InitTeleport();
+        }
+
+        if (nextTelpoNum)
+        {
+            nextTelpoNum = false;
+            telpoNum++;
+            InitTeleport();
+        }        
+    }
+
+    public void InitPoint()
+    {
+        if (pointGroup.Count > 0)
+        {
+            if (pointNum >= pointGroup.Count)
+                pointNum = 0;
+            else if (pointNum < 0)
+                pointNum = pointGroup.Count - 1;
+
+            point = pointGroup[pointNum];
+        }
+    }
+    public void InitTeleport()
+    {
+        if (pointGroup.Count > 0)
+        {
+            if (telpoNum >= pointGroup.Count)
+                telpoNum = 0;
+            else if (telpoNum < 0)
+                telpoNum = pointGroup.Count - 1;
+
+            teleportPoint = pointGroup[telpoNum];
+        }
     }
 }
