5e22f9bea8c5e1714aeced2ff9de1090a5dac55a
sin49 <ngo533gute@gmail.com>
2024-08-28 09:28:32 +0900

    12313
    23132131321

--- Assets/Scripts/ProtoType/Character/Character.cs
+++ Assets/Scripts/ProtoType/Character/Character.cs
@@ -8,11 +8,16 @@ public abstract class Character: MonoBehaviour
     protected CharacterSoundPlayer soundplayer;
     event Action hittedevent;
     event Action attackevent;
+    event Action moveevent;
+    event Action deadevent;
     public void registerhittedevent(Action a)
     {
         hittedevent += a;
     }
-   
+   public void registerdeadevent(Action a)
+    {
+        deadevent += a;
+    }
     public abstract void Attack();
     public virtual void Damaged(float damage)
     {
