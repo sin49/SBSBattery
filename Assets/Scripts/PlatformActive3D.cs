e2eafdc8a055cb4fa871b719cada4abdff7db639
sin49 <ngo533gute@gmail.com>
2024-09-04 14:14:07 +0900

    132131
    2312323

--- Assets/Scripts/PlatformActive3D.cs
+++ Assets/Scripts/PlatformActive3D.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class PlatformActive3D : MonoBehaviour
 {
     Collider Bcollider;
+    MeshRenderer renderer_;
     public float Zmove = 1;
     public bool BcolliderActive3D=true;
     private void Awake()
     {
         Bcollider = GetComponent<BoxCollider>();
+        renderer_=GetComponent<MeshRenderer>();
     }
     void PlatformChange3D()
     {
-        if (BcolliderActive3D) 
-        Bcollider.enabled = true;
+        if (BcolliderActive3D)
+        {
+            Bcollider.enabled = true;
+            renderer_.enabled = true;
+        }
         else
+        {
             Bcollider.enabled = false;
+        renderer_.enabled = false;
+        }
         transform.Translate(Vector3.back * Zmove);
     }
     void PlatformChange2D()
     {
         if (BcolliderActive3D)
+        {
             Bcollider.enabled = false;
+            renderer_.enabled = false;
+        }
         else
+        {
             Bcollider.enabled = true;
+            renderer_.enabled = true;
+        }
         transform.Translate(Vector3.forward * Zmove);
     }
 
