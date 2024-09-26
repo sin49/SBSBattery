827175c2802c7244e050481178bb94c9b6739610
LDJ <ldj4689@naver.com>
2024-09-26 14:32:43 +0900

    0926dddd
    sdsdsd

---
+++ Assets/JMO Assets/Welcome Screen/CFXR_WelcomeScreen.cs
@@ -0,0 +1,12 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CartoonFX
+{
+    [InitializeOnLoad]
+    public class CFXR_WelcomeScreen : EditorWindow
+    {
+
+    }
+}
