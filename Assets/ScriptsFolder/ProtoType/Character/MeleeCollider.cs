0513acf654e9890e4d58d6f25ec901e8764e6392
sin49 <ngo533gute@gmail.com>
2024-10-08 23:55:23 +0900

    12345
    123456

--- Assets/ScriptsFolder/ProtoType/Character/MeleeCollider.cs
+++ Assets/ScriptsFolder/ProtoType/Character/MeleeCollider.cs
@@ -10,6 +10,7 @@ public class MeleeCollider : PlayerAttack
 
     private void Start()
     {
+        if(hitEffect!=null)
         saveEffect = Instantiate(hitEffect).GetComponent<ParticleSystem>();
         damage = PlayerStat.instance.atk;
         gameObject.SetActive(false);
@@ -17,8 +18,10 @@ public class MeleeCollider : PlayerAttack
     public override void DamageCollider(Collider other)
     {
         base.DamageCollider(other);
-
-        saveEffect.transform.position = new(other.transform.position.x, other.transform.position.y + .5f, other.transform.position.z);
-        saveEffect.Play();
+        if (saveEffect != null)
+        {
+            saveEffect.transform.position = new(other.transform.position.x, other.transform.position.y + .5f, other.transform.position.z);
+            saveEffect.Play();
+        }
     }
 }
