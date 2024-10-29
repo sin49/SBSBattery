031ce847a8cc1fc5fcb2811e686df3bcdfff9772
sin49 <ngo533gute@gmail.com>
2024-10-30 06:05:11 +0900

    1231
    3123313

--- Assets/ScriptsFolder/ProtoType/EnemyScripts/EnemyTrackingAndPatrol.cs
+++ Assets/ScriptsFolder/ProtoType/EnemyScripts/EnemyTrackingAndPatrol.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
 
 public class EnemyTrackingAndPatrol : MonoBehaviour
 {
